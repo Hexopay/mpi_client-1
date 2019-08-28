1bf86933b230f6e8350c02802b7284d6ecc203c7
Pavel <ba1ashpash@gmail.com>
2019-08-28 14:07:25 +0300

    Update remote specs to be workable

--- spec/remote/verification_spec.rb
+++ spec/remote/verification_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'test of verification request' do
   it "should have status 'N'" do
-    req = Verification::Request.new(request_params(:card_number => '4012001038443335'), '1')
+    req = Verification::Request.new(request_params(card_number: '4012001038443335'), '1')
     response = req.process
     response.status.should == 'N'
   end
 
   it "should return 'Y' and url" do
-    req = Verification::Request.new(request_params(:card_number => '4200000000000000'), '2')
+    req = Verification::Request.new(request_params, '2')
     response = req.process
 
     response.status.should == 'Y'
@@ -18,16 +18,17 @@ describe 'test of verification request' do
     response.acs_url.should_not be_empty
   end
 
-  def request_params(new_params)
-    { :account_id       => 'd6d8479f23a526b4e676c88759abd27a',
+  def request_params(new_params = {})
+    {
+      :account_id       => '246064ab504d39136c99bcb0b8d8b4f7',
       :amount           => '100',
-      :card_number      => '4200000000000000',
+      :card_number      => '4012001037141112',
       :description      => 'Test order',
       :display_amount   => '1 USD',
       :currency         => '840',
       :exp_month        => '12',
       :exp_year         => '16',
-      :termination_url  => 'http://termurl.com'
+      :termination_url  => 'http://50acb15a.ngrok.io'
     }.update(new_params)
   end
 end
